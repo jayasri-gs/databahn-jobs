d967de652b62539a4bed4f05ac716dc0115aa978
Nilesh Injulkar <ninjulkar@databahn.ai>
2024-01-25 14:02:22 +0530

    merge conflict resolved

--- internal/common/constants.go
+++ internal/common/constants.go
@@ -1,5 +1,5 @@
 package common
 
-const INSIGHTS_AGGREGATION = "insights-aggregation"
+const INSIGHTS_AGGREGATION = "insights_aggregation"
 const DATA_REPLAY = "data-replay"
 const DEVICE_INVENTORY_HEALTH = "device_inventory_health"
