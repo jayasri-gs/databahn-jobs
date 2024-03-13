d69766a8df07f9b8cf526422e86849a1d9560fe6
akshad <aagrawal@databahn.ai>
2024-03-13 14:43:35 +0530

    delete unused function

--- internal/healthchecker/helper/alerts.go
+++ internal/healthchecker/helper/alerts.go
@@ -10,32 +10,6 @@ import (
 	"time"
 )
 
-func SendDismissALertsToControlFlag(ctx context.Context, toDismissAlerts []alerts_common.AlertEntityObject) error {
-	var alerts []alerts_common.Alert
-	for _, entity := range toDismissAlerts {
-		temp := alerts_common.Alert{
-			FunctionalityEntityId: entity.EntityId.String(),
-			Dismissed:             true,
-			Status:                alerts_common.AlertAutoResolved,
-			UpdatedAt:             time.Now(),
-			UpdatedBy:             "system",
-		}
-		alerts = append(alerts, temp)
-	}
-
-	alertClient, err := alert.NewAlertClient(ctx, config.GetAppConfiguration())
-	if err != nil {
-		logger.GetLogger().Error("error while creating alert client", zap.Error(err))
-		return err
-	}
-	resp, err := alertClient.SendAlertWithRetry(alert.Request{Alerts: alerts}, 3)
-	if err != nil {
-		return err
-	}
-	logger.GetLogger().Info("alert dismissed successfully", zap.Any("response", resp))
-
-	return nil
-}
 func SendAlertToControlFlag(ctx context.Context, entityArray []alerts_common.AlertEntityObject, title string, message string, functionalityType string, functionality string, severity string, status int, dismissed bool, updatedBy string) error {
 	var alerts []alerts_common.Alert
 	for _, entity := range entityArray {
