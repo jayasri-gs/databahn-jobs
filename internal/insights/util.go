3d114d146493d1e0fa0a332d35d23fed5011c5be
Abhishek Mundada <amundada@databahn.ai>
2024-05-14 22:00:56 +0530

    insights to support 5 attributes

--- internal/insights/util.go
+++ internal/insights/util.go
@@ -5,8 +5,8 @@ import (
 	"net/url"
 )
 
-func InsightId(key1, key2, sourceId string) string {
-	return fmt.Sprintf("%s:%s:%s", url.QueryEscape(key1), url.QueryEscape(key2), sourceId)
+func InsightId(key1, key2, key3, key4, key5, sourceId string) string {
+	return fmt.Sprintf("%s:%s:%s:%s:%s:%s", url.QueryEscape(key1), url.QueryEscape(key2), url.QueryEscape(key3), url.QueryEscape(key4), url.QueryEscape(key5), sourceId)
 }
 
 func SightIndexName(tenantId string) string {
