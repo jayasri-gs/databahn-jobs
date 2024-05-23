488cbd764ef42d403df07ca30bf79470b81218a2
Nilesh Injulkar <ninjulkar@databahn.ai>
2024-05-23 16:23:35 +0530

    insight id fix

--- internal/insights/util.go
+++ internal/insights/util.go
@@ -9,10 +9,6 @@ func InsightId(key1, key2, key3, key4, key5, sourceId string) string {
 	return fmt.Sprintf("%s:%s:%s:%s:%s:%s", url.QueryEscape(key1), url.QueryEscape(key2), url.QueryEscape(key3), url.QueryEscape(key4), url.QueryEscape(key5), sourceId)
 }
 
-func InsightId2(key1, key2, sourceId string) string {
-	return fmt.Sprintf("%s:%s:%s", url.QueryEscape(key1), url.QueryEscape(key2), sourceId)
-}
-
 func SightIndexName(tenantId string) string {
 	return fmt.Sprintf("%s%s_%s_%s", INSIGHTS_STORE_INDEX_PREFIX, AGG_SIGHTS, APP_TYPE_SOURCEHOSTNAME, tenantId)
 }
