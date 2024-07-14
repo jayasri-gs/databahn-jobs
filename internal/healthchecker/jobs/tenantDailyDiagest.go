6061cfcfbe63c8ace9231f218b3849a1dd68fa5f
Aman-Pailwan <aman.pailwan.773@gmail.com>
2024-07-14 15:35:07 +0530

    Daily Digest Updates

--- internal/healthchecker/jobs/tenantDailyDiagest.go
+++ internal/healthchecker/jobs/tenantDailyDiagest.go
@@ -3,6 +3,11 @@ package jobs
 import (
 	"context"
 	"encoding/json"
+	"io"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/databahn-ai/databahn-jobs/internal/config"
 	"github.com/databahn-ai/databahn-jobs/internal/store/os"
 	"github.com/databahn-ai/databahn-jobs/internal/store/statistics"
@@ -10,15 +15,16 @@ import (
 	"github.com/databahn-ai/go-logging/logger"
 	"github.com/opensearch-project/opensearch-go/v2"
 	"go.uber.org/zap"
-	"io"
-	"strconv"
-	"strings"
-	"time"
 )
 
 func TenantDailyDigest(ctx context.Context) error {
 	conf := os.GetConf()
 	client, err := os.NewClient(ctx, conf.Url, conf.Creds())
+	if err != nil {
+		logger.GetLogger().Error("error while connecting to statistics store", zap.Error(err))
+		return err
+	}
+
 	startTime := strconv.Itoa(int(time.Now().Add(-24 * time.Hour).UnixMilli()))
 	endTime := strconv.Itoa(int(time.Now().UnixMilli()))
 
@@ -26,17 +32,33 @@ func TenantDailyDigest(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
 	eventsIngestedByTenantId, err := getTotalEventsIngested(ctx, client, startTime, endTime)
 	if err != nil {
 		logger.GetLogger().Error("error while getting total events ingested", zap.Error(err))
 		return err
 	}
-	sizeIngestedByTenantId, err := getTotalDataReceived(ctx, client, startTime, endTime)
+
+	dataReceivedByTenantId, err := getTotalDataReceived(ctx, client, startTime, endTime)
 	if err != nil {
 		logger.GetLogger().Error("error while getting total data received", zap.Error(err))
 		return err
 	}
+
+	sensitiveDataByTenantId, err := sensitiveDataTracking(ctx, client, startTime, endTime)
+	if err != nil {
+		logger.GetLogger().Error("error while getting sensitive data tracking", zap.Error(err))
+		return err
+	}
+
+	dataDeliveredByTenantId, err := getTotalDataDelivered(ctx, client, startTime, endTime)
+	if err != nil {
+		logger.GetLogger().Error("error while getting total data delivered", zap.Error(err))
+		return err
+	}
+
 	logger.GetLogger().Info("got total events ingested", zap.Reflect("eventsByTenantId", eventsIngestedByTenantId))
+
 	for _, currTenant := range t {
 		logger.GetLogger().Info("Processing tenant "+currTenant.Id.String(), zap.String("name", currTenant.Name))
 		dailyDigest := tenant.Digest{
@@ -52,14 +74,33 @@ func TenantDailyDigest(ctx context.Context) error {
 			dailyDigest.IngestionHealth = "Healthy"
 		}
 
-		if sizeIngestedByTenantId.Agg[currTenant.Id.String()] == nil {
+		if dataReceivedByTenantId.Agg[currTenant.Id.String()] == nil {
 			dailyDigest.TotalIngestionSize = 0
 		} else {
-			dailyDigest.TotalIngestionSize = sizeIngestedByTenantId.Agg[currTenant.Id.String()].(float64)
+			dailyDigest.TotalIngestionSize = dataReceivedByTenantId.Agg[currTenant.Id.String()].(float64)
+		}
+
+		if dataDeliveredByTenantId.Agg[currTenant.Id.String()] == nil {
+			dailyDigest.TotalDeliveredSize = 0
+		} else {
+			dailyDigest.TotalDeliveredSize = dataDeliveredByTenantId.Agg[currTenant.Id.String()].(float64)
+		}
+
+		if sensitiveDataByTenantId.Agg[currTenant.Id.String()] == nil {
+			dailyDigest.SensitiveDataEvents = 0
+		} else {
+			dailyDigest.SensitiveDataEvents = sensitiveDataByTenantId.Agg[currTenant.Id.String()].(float64)
+		}
+
+		if dailyDigest.TotalIngestionSize > 0 {
+			dailyDigest.VolumeReductionAchievement = ((dailyDigest.TotalIngestionSize - dailyDigest.TotalDeliveredSize) / dailyDigest.TotalIngestionSize) * 100
+		} else {
+			dailyDigest.VolumeReductionAchievement = 0
 		}
 
 		logger.GetLogger().Info("daily digest for tenant", zap.String("tenantId", currTenant.Id.String()), zap.String("tenantName", currTenant.Name), zap.Reflect("digest", dailyDigest))
 	}
+
 	return nil
 }
 
@@ -99,6 +140,75 @@ func getTotalEventsIngested(ctx context.Context, client *opensearch.Client, star
 
 func getTotalDataReceived(ctx context.Context, client *opensearch.Client, startTime, endTime string) (*statistics.AggregateResponse, error) {
 	q := `tags.component_name: "storage" AND name: "total_data_received"`
+	query := statistics.AddDateRange(q, startTime, endTime)
+	agg := "tags.db_tenant_id.keyword"
+	conf := os.GetConf()
+	client, err := os.NewClient(ctx, conf.Url, conf.Creds())
+	if err != nil {
+		logger.GetLoggerWithContext(ctx).Error("error while connecting to statistics store", zap.Error(err))
+		return nil, err
+	}
+
+	searchBody := &statistics.AggregateQueryRequest{}
+	searchBody.Size = 0
+	searchBody.Query.QueryString.Query = query
+
+	aggList := strings.Split(agg, ",")
+	searchBody.NestedAgg = statistics.BuildNextAggregation(aggList, 0)
+
+	searchResponse, err := os.MakeSearchCall(ctx, conf.StatsIndex+"*", &searchBody, client)
+
+	if err != nil {
+		logger.GetLoggerWithContext(ctx).Error("error while querying to statistics store", zap.Error(err), zap.String("url", conf.Url), zap.String("index", conf.StatsIndex))
+		return nil, err
+	}
+
+	bodyContent, _ := io.ReadAll(searchResponse.Body)
+
+	resp := &statistics.AggregateQueryResponse{}
+	err = json.Unmarshal(bodyContent, resp)
+	aggObj := statistics.NewAggregateResponse(resp)
+	logger.GetLoggerWithContext(ctx).Info("got response from statistics store")
+	return &aggObj, err
+}
+
+func sensitiveDataTracking(ctx context.Context, client *opensearch.Client, startTime, endTime string) (*statistics.AggregateResponse, error) {
+	q := `tags.sensitive_type.keyword AND name: "sensitive_total"`
+
+	query := statistics.AddDateRange(q, startTime, endTime)
+	agg := "tags.db_tenant_id.keyword"
+	conf := os.GetConf()
+	client, err := os.NewClient(ctx, conf.Url, conf.Creds())
+	if err != nil {
+		logger.GetLoggerWithContext(ctx).Error("error while connecting to statistics store", zap.Error(err))
+		return nil, err
+	}
+
+	searchBody := &statistics.AggregateQueryRequest{}
+	searchBody.Size = 0
+	searchBody.Query.QueryString.Query = query
+
+	aggList := strings.Split(agg, ",")
+	searchBody.NestedAgg = statistics.BuildNextAggregation(aggList, 0)
+
+	searchResponse, err := os.MakeSearchCall(ctx, conf.StatsIndex+"*", &searchBody, client)
+
+	if err != nil {
+		logger.GetLoggerWithContext(ctx).Error("error while querying to statistics store", zap.Error(err), zap.String("url", conf.Url), zap.String("index", conf.StatsIndex))
+		return nil, err
+	}
+
+	bodyContent, _ := io.ReadAll(searchResponse.Body)
+
+	resp := &statistics.AggregateQueryResponse{}
+	err = json.Unmarshal(bodyContent, resp)
+	aggObj := statistics.NewAggregateResponse(resp)
+	logger.GetLoggerWithContext(ctx).Info("got response from statistics store")
+	return &aggObj, err
+}
+
+func getTotalDataDelivered(ctx context.Context, client *opensearch.Client, startTime, endTime string) (*statistics.AggregateResponse, error) {
+	q := `tags.component_name: "dispenser" AND name: "total_events_delivered"`
 	query := statistics.AddDateRange(q, startTime, endTime)
 	agg := "tags.db_tenant_id.keyword"
 	conf := os.GetConf()
--- internal/store/tenant/dailyDigest.go
+++ internal/store/tenant/dailyDigest.go
@@ -3,15 +3,18 @@ package tenant
 import "github.com/google/uuid"
 
 type Digest struct {
-	TenantId             uuid.UUID          `json:"tenant_id"`
-	Name                 string             `json:"name"`
-	IngestionHealth      string             `json:"ingestion_health"`
-	DeliveryHealth       string             `json:"device_health"`
-	TotalIngestionEvents float64            `json:"total_ingestion_events"`
-	TotalIngestionSize   float64            `json:"total_ingestion_size"`
-	AverageEPS           int64              `json:"average_eps"`
-	DeliveryBreakdown    map[string]float64 `json:"delivery_breakdown"`
-	SensitiveData        map[string]float64 `json:"sensitive_data"`
-	ReductionPercentage  map[string]int     `json:"reduction_percentage"`
-	Alerts               []string           `json:"alerts"`
+	TenantId                   uuid.UUID          `json:"tenant_id"`
+	Name                       string             `json:"name"`
+	IngestionHealth            string             `json:"ingestion_health"`
+	DeliveryHealth             string             `json:"device_health"`
+	TotalIngestionEvents       float64            `json:"total_ingestion_events"`
+	TotalIngestionSize         float64            `json:"total_ingestion_size"`
+	TotalDeliveredSize         float64            `json:"total_delivered_size"`
+	AverageEPS                 int64              `json:"average_eps"`
+	DeliveryBreakdown          map[string]float64 `json:"delivery_breakdown"`
+	SensitiveData              map[string]float64 `json:"sensitive_data"`
+	ReductionPercentage        map[string]int     `json:"reduction_percentage"`
+	Alerts                     []string           `json:"alerts"`
+	SensitiveDataEvents        float64            `json:"sensitive_data_events"`
+	VolumeReductionAchievement float64            `json:"volume_reduction_achievement"`
 }
