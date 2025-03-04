5fd29ded887ab7a075e2b3b090c2c9b7ef7c6206
Aman-Pailwan <apailwan@databahn.ai>
2025-03-04 18:49:38 +0530

    logging for config log sources

--- internal/healthchecker/jobs/logsourceReputationChecker.go
+++ internal/healthchecker/jobs/logsourceReputationChecker.go
@@ -81,7 +81,7 @@ func UpdateReputationForLogSources(ctx context.Context) error {
 		return err
 	}
 
-	logging.GetLogger().Info("config log sources", zap.Any("configlogSources", configLogSources))
+	logging.GetLogger().Info("config log sources", zap.Any("config logSources", configLogSources))
 
 	endTime := time.Now()
 	startTime := endTime.Add(-time.Hour * time.Duration(util.GetEnvInt64FromString(healthchecker.ReputationCheckerTime)))
