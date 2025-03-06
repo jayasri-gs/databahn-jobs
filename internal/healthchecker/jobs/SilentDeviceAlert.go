f56eee9dfe2b05f561b7acaf448d422b622e222b
Aman-Pailwan <apailwan@databahn.ai>
2025-03-06 05:35:10 +0530

    Send email logic added

--- internal/healthchecker/jobs/SilentDeviceAlert.go
+++ internal/healthchecker/jobs/SilentDeviceAlert.go
@@ -2,8 +2,11 @@ package jobs
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/databahn-ai/common-utils/aws"
 	"github.com/databahn-ai/databahn-jobs/internal/config"
+	awsemail "github.com/databahn-ai/databahn-jobs/internal/healthchecker/aws"
 	"github.com/databahn-ai/databahn-jobs/internal/store/os"
 	"github.com/databahn-ai/databahn-jobs/internal/store/statistics"
 	"github.com/databahn-ai/databahn-jobs/internal/store/tenant"
@@ -18,17 +21,19 @@ import (
 )
 
 type Device struct {
-	Hostname string `json:"hostname"`
-	MinTime  int64  `json:"min_time"`
-	MaxTime  int64  `json:"max_time"`
-	SourceID string `json:"source_id"`
-	TenantId string `json:"tenant_id"`
+	Hostname   string `json:"hostname"`
+	MinTime    int64  `json:"min_time"`
+	MaxTime    int64  `json:"max_time"`
+	SourceID   string `json:"source_id"`
+	TenantId   string `json:"tenant_id"`
+	TenantName string `json:"tenant_name"`
+	Summary    string `json:"summary,omitempty"`
 }
 type CollectionProfile struct {
 	LogSourceId uuid.UUID `gorm:"column:log_source_id"`
 }
 
-func getSilentDevices(ctx context.Context, client *opensearch.Client, index string, query string, pageSize int, searchAfter []any) ([]Device, []any, error) {
+func getSilentDevices(ctx context.Context, client *opensearch.Client, index string, query string, pageSize int, searchAfter []any, tenantName string) ([]Device, []any, error) {
 	var silentDevices []Device
 	res, newSearchAfter, err := os.SearchPaginated(ctx, client, index, query, pageSize, searchAfter, []os.Sort{{Field: "max_time", Order: "desc"}})
 	if err != nil {
@@ -48,11 +53,12 @@ func getSilentDevices(ctx context.Context, client *opensearch.Client, index stri
 
 	for _, device := range deviceInventoryList {
 		silentDevices = append(silentDevices, Device{
-			Hostname: device.Hostname,
-			MinTime:  device.MinTime,
-			MaxTime:  device.MaxTime,
-			SourceID: device.SourceId,
-			TenantId: device.TenantId,
+			Hostname:   device.Hostname,
+			MinTime:    device.MinTime,
+			MaxTime:    device.MaxTime,
+			SourceID:   device.SourceId,
+			TenantId:   device.TenantId,
+			TenantName: tenantName,
 		})
 	}
 
@@ -71,20 +77,28 @@ func ProcessSilentDevices(ctx context.Context) error {
 	}
 
 	for _, t := range tenants {
-		// Fetch silent devices
-		silentDevices, err := FetchSilentDevices(ctx, t.Id.String(), logSourceIds)
+
+		silentDevices, err := FetchSilentDevices(ctx, t.Id.String(), t.Name, logSourceIds)
 		if err != nil {
 			return fmt.Errorf("failed to fetch silent devices: %w", err)
 		}
-		if len(silentDevices) == 0 {
+
+		if len(silentDevices) > 0 {
+			logging.GetLoggerWithContext(ctx).Info("silent devices found", zap.String("tenantId", t.Id.String()), zap.Int("count", len(silentDevices)))
+		} else {
 			logging.GetLogger().Info("no silent devices found", zap.String("tenantId", t.Id.String()))
 			continue
+		}
+
+		if err := sendAlertsForSilentDevices(ctx, silentDevices); err != nil {
+			logging.GetLoggerWithContext(ctx).Error("error sending alerts for silent devices", zap.String("tenantId", t.Id.String()), zap.Error(err))
+			return fmt.Errorf("failed to send alerts for silent devices: %w", err)
 		}
 	}
 
 	return nil
 }
-func FetchSilentDevices(ctx context.Context, tenantId string, sources []string) ([]Device, error) {
+func FetchSilentDevices(ctx context.Context, tenantId string, tenantName string, sources []string) ([]Device, error) {
 	conf := os.GetConf()
 	client, err := os.NewClient(ctx, conf.Url, conf.Creds())
 	if err != nil {
@@ -105,7 +119,7 @@ func FetchSilentDevices(ctx context.Context, tenantId string, sources []string) 
 
 	var allSilentDevices []Device
 	for {
-		silentDevices, newSearchAfter, err := getSilentDevices(ctx, client, index, query, pageSize, searchAfter)
+		silentDevices, newSearchAfter, err := getSilentDevices(ctx, client, index, query, pageSize, searchAfter, tenantName)
 		if err != nil {
 			logging.GetLoggerWithContext(ctx).Error("error while querying to statistics store", zap.Error(err), zap.String("url", conf.Url), zap.String("index", index))
 			return nil, err
@@ -157,4 +171,40 @@ func GetLogSourceIdsFromCollectionProfile(ctx context.Context) ([]string, error)
 		logSourceIds = append(logSourceIds, profile.LogSourceID)
 	}
 	return logSourceIds, nil
+}
+
+func sendAlertsForSilentDevices(ctx context.Context, silentDevices []Device) error {
+
+	for _, configObject := range silentDevices {
+
+		var emailTo []string
+		emailTo = append(emailTo, config.GetAppConfiguration().GetString(awsemail.OPSGini))
+		configObject.Summary = fmt.Sprintf("No data received for the last 24 hours (1440 minutes)")
+		formattedConfigObject, err := json.MarshalIndent(configObject, "", "  ")
+		if err != nil {
+			logging.GetLoggerWithContext(ctx).Error("error marshaling configObject to JSON", zap.Error(err))
+			continue
+		}
+
+		title := fmt.Sprintf("Device :%s:%s", configObject.Hostname, configObject.TenantName)
+		var email = awsemail.EmailNotification{
+			Recipients: &awsemail.Recipient{
+				To: emailTo,
+			},
+			Body:    aws.String("<pre>" + string(formattedConfigObject) + "</pre>"),
+			Subject: aws.String(title),
+		}
+
+		err = awsemail.SendEmail(ctx, email)
+		if err != nil {
+			logging.GetLoggerWithContext(ctx).Info("Error sending notification")
+			return err
+		}
+
+		logging.GetLoggerWithContext(ctx).Info("sent notification")
+
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	return nil
 }
