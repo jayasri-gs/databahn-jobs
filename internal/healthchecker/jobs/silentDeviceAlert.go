d1f6842ae63ca18c15ed85926d050c438d0403c5
Aman-Pailwan <apailwan@databahn.ai>
2025-05-29 00:55:14 +0530

    Added Loggers for debug

--- internal/healthchecker/jobs/silentDeviceAlert.go
+++ internal/healthchecker/jobs/silentDeviceAlert.go
@@ -95,6 +95,7 @@ func getSilentDevices(ctx context.Context, client *opensearch.Client, index stri
 			TenantName: tenantName,
 		})
 	}
+	logging.GetLoggerWithContext(ctx).Info("silent devices fetched", zap.Int("count", len(silentDevices)))
 
 	return silentDevices, newSearchAfter, nil
 }
@@ -147,6 +148,8 @@ func FetchSilentDevices(ctx context.Context, tenantId string, tenantName string,
 		return nil, err
 	}
 
+	logging.GetLoggerWithContext(ctx).Info("query built", zap.String("query", query))
+
 	var searchAfter []any
 	pageSize := 100
 	index := "db_insights_sights_sourcehostname_" + tenantId
@@ -167,6 +170,8 @@ func FetchSilentDevices(ctx context.Context, tenantId string, tenantName string,
 		searchAfter = newSearchAfter
 	}
 
+	logging.GetLoggerWithContext(ctx).Info("silent devices fetched", zap.Any("silentDevices", allSilentDevices))
+
 	return allSilentDevices, nil
 }
 
@@ -176,8 +181,10 @@ func getQueryFromFilters(sources []string, tenantId string) (string, error) {
 		q += ` AND source_id: ` + "(" + strings.Join(sources, " OR ") + ")"
 	}
 
-	// Set endTime to 24 hours before the current time (previous day)
+	// Set endTime to 4 hours before the current time (previous day)
 	endTime := time.Now().Add(-4 * time.Hour).Format(time.RFC3339)
+
+	logging.GetLogger().Info("endTime", zap.String("endTime", endTime))
 
 	// Parse endTime and add it to the query
 	t, err := time.Parse(time.RFC3339, endTime)
@@ -185,6 +192,9 @@ func getQueryFromFilters(sources []string, tenantId string) (string, error) {
 		return "", fmt.Errorf("error parsing endTime: %v", err)
 	}
 	endTimeEpoch := t.UnixMilli()
+
+	logging.GetLogger().Info("endTimeEpoch", zap.Int64("endTimeEpoch", endTimeEpoch))
+
 	q += ` AND max_time:<` + strconv.FormatInt(endTimeEpoch, 10)
 
 	return q, nil
