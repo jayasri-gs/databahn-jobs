d97fca3b811f3efdfdbb62c3f3cbf0e1f1abc011
nitin-databahn <ndhiman@databahn.ai>
2025-10-27 12:14:36 +0530

    fix(process): update document ID generation logic to use Key1 for sourcehostname

--- internal/insights/process.go
+++ internal/insights/process.go
@@ -100,6 +100,8 @@ const sightsScript = `
       ctx._source.source_id = params.source_id; 
       ctx._source.timestamp = params.timestamp;
       ctx._source.updated_at = params.updated_at;
+      ctx._source.key1 = params.key1;
+      ct
     ",
     "lang": "painless",
     "params": {
@@ -250,7 +252,7 @@ func aggregateInsights(ctx context.Context, cli *opensearch.Client, index IndexM
 			if index.Type == "sourcehostname" {
 				// need to use key 3 (FQDN) instead of small otherwise duplicate documents will be created
 				// keeping key generation logic same as before for backward compatibility
-				doc.Id = InsightId(bucket.Key.Key3, "", "", bucket.Key.Key4, bucket.Key.Key5, bucket.Key.SourceId)
+				doc.Id = InsightId(bucket.Key.Key1, "", "", bucket.Key.Key4, bucket.Key.Key5, bucket.Key.SourceId)
 			} else {
 				doc.Id = InsightId(bucket.Key.Key1, bucket.Key.Key2, bucket.Key.Key3, bucket.Key.Key4, bucket.Key.Key5, bucket.Key.SourceId)
 			}
