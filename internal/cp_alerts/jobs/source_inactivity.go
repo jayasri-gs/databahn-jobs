66d70efc72d46bbeadc046899b2eb5d9baad51f9
Avinash Jagdale <ajagdale@databahn.ai>
2026-06-09 09:42:54 +0530

    [DB-27533] Skip agent source alert when alertConfig null

--- internal/cp_alerts/jobs/source_inactivity.go
+++ internal/cp_alerts/jobs/source_inactivity.go
@@ -325,8 +325,12 @@ func findInactiveAndActiveSources(db *gorm.DB, tenantUuid uuid.UUID, sourceIdToL
 				}
 				alertDuration = configuredDuration
 			} else {
-				logger.GetLogger().Warn("no alert config found for source, skipping", zap.String("sourceId", sourceId), zap.String("tenantId", tenantId))
-				continue
+				if s.Scope == "AGENT" { // [DB-27533]Skip agent alert if no alert config found for source. Remove this after source alert added in collection profile UI.
+					logger.GetLogger().Warn("no alert config found for agent source, skipping alert", zap.String("sourceId", sourceId), zap.String("tenantId", tenantId))
+					continue
+				}
+				logger.GetLogger().Warn("no alert config found for source, defaulting", zap.String("sourceId", sourceId), zap.String("tenantId", tenantId))
+				alertDuration = defaultAlertDuration30Min
 			}
 
 			fleets := sourceToFleets[sourceId]
